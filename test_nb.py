3f8a4fea3cf403d6bfaacc88429ccbe282490288
KangWon LEE <kangwon.lee@kpu.ac.kr>
2017-10-22 04:26:01 +0900

    added check_kernel_spec() ; https://jupyter-client.readthedocs.io/en/latest/api/kernelspec.html; --ExecutePreprocessor.kernel_name to python # ijstokes et al, Command line execution of a jupyter notebook fails in default Anaconda 4.1, https://github.com/Anaconda-Platform/nb_conda_kernels/issues/34

--- test_nb.py
+++ test_nb.py
@@ -6,12 +6,21 @@ import subprocess
 import tempfile
 
 
+def check_kernel_spec():
+    # https://jupyter-client.readthedocs.io/en/latest/api/kernelspec.html
+    import jupyter_client.kernelspec as jk
+    kernel_spec_manager = jk.KernelSpecManager()
+
+    print(kernel_spec_manager.get_all_specs())
+
+
 def _exec_notebook(path):
     # http://nbconvert.readthedocs.io/en/latest/execute_api.html
+    # ijstokes et al, Command line execution of a jupyter notebook fails in default Anaconda 4.1, https://github.com/Anaconda-Platform/nb_conda_kernels/issues/34
     with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
         args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
                 "--ExecutePreprocessor.timeout=1000",
-                "--ExecutePreprocessor.kernel_name=python3",
+                "--ExecutePreprocessor.kernel_name=python",
                 "--output", fout.name, path]
         subprocess.check_call(args)
 
