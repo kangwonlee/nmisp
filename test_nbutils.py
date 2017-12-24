5a1d09d59438d7c22af3935149c988b081880969
KangWon Lee <kangwon.lee@kpu.ac.kr>
2017-12-24 21:45:27 +0900

    started test_replace_symbol() : test cases for file level replace results

--- test_nbutils.py
+++ test_nbutils.py
@@ -38,6 +38,24 @@ class TestNButils(unittest.TestCase):
 
         self.assertSequenceEqual(expected, result)
 
+    def test_replace_symbol(self):
+        file = nbutils.read_file(self.input_file_name)
+        cells = file['cells']
+        result = []
+        for k, cell in enumerate(cells):
+            cell_result = nbutils.has_symbol(cell)
+            if cell_result:
+                result.append((k, cell_result))
+
+        # compare with an expected list
+        expected = [(8, [(0, "L_AB_m = sy.symbols('L_{AB}[m]', real=True, nonnegative=True)")]),
+                    (10, [(0, "w0_N_m = sy.symbols('w0[N/m]', real=True)")]),
+                    (12, [(0, "E_Pa, I_m4 = sy.symbols('E[Pa], I[m^{4}]', positive=True)")]),
+                    (14, [(0, "x_m = sy.symbols('x[m]', nonnegative=True)")]),
+                    (16, [(0, "R_A_N, M_A_Nm, R_B_N = sy.symbols('R_{A}[N], M_{A}[Nm], R_{B}[N]', real=True)")])]
+
+        self.assertSequenceEqual(expected, result)
+
 
 def _exec_notebook(path):
     # http://nbconvert.readthedocs.io/en/latest/execute_api.html
