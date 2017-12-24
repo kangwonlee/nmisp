fc69caab253776a0dfa61a2fc2e25d8828257292
KangWon Lee <kangwon.lee@kpu.ac.kr>
2017-12-24 21:10:44 +0900

    removed with block

--- tests/test_nbutils.py
+++ tests/test_nbutils.py
@@ -16,11 +16,10 @@ class TestNButils(unittest.TestCase):
 def _exec_notebook(path):
     # http://nbconvert.readthedocs.io/en/latest/execute_api.html
     # ijstokes et al, Command line execution of a jupyter notebook fails in default Anaconda 4.1, https://github.com/Anaconda-Platform/nb_conda_kernels/issues/34
-    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
-        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
-                "--ExecutePreprocessor.timeout=1000",
-                "--ExecutePreprocessor.kernel_name=python", path]
-        subprocess.check_call(args)
+    args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+            "--ExecutePreprocessor.timeout=1000",
+            "--ExecutePreprocessor.kernel_name=python", path]
+    subprocess.check_call(args)
 
 
 if __name__ == '__main__':
