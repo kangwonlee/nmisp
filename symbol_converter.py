405c573bf2371a5daf5efdaf208639e3b46ad9ec
KangWon Lee <kangwon.lee@kpu.ac.kr>
2017-12-26 02:48:49 +0900

    started class SymbolConverter

--- symbol_converter.py
+++ symbol_converter.py
@@ -21,6 +21,32 @@ class SymbolLister(fu.CellProcessorBase):
         return self.has_symbol()
 
 
+class SymbolConverter(SymbolLister):
+    units_set = {'m', 'mm', 'mm3', 'm2', 'm3', 'm4', 'deg', 'rad', 'N', 'Nm', 'N_m', 'Pa', 'MPa', 'm_s2', 'kg'}
+
+    @staticmethod
+    def wrap_symbol_name(symbol_name):
+        """
+        Wrap '_' separated symbol name parts with '{}'
+        :param str symbol_name:
+        :return:
+
+        Example
+        >>> cp = SymbolConverter()
+        >>> cp.wrap_symbol_name('L_AB_m')
+        'L_{AB}_{m}'
+        """
+        symbol_name_split_under_line = symbol_name.split('_')
+        if 1 < len(symbol_name_split_under_line):
+            symbol_name_underline_wrapped = [symbol_name_split_under_line[0]]
+            for part in symbol_name_split_under_line[1:]:
+                symbol_name_underline_wrapped.append('{%s}' % part)
+
+            symbol_name = '_'.join(symbol_name_underline_wrapped)
+
+        return symbol_name
+
+
 def symbol_lines_in_file(input_file_name):
     sc = SymbolLister()
     file_processor = fu.FileProcessor(input_file_name, sc)
