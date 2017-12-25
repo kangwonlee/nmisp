8f2651cd76e2068459b78b54361c5916bce78cc1
KangWon Lee <kangwon.lee@kpu.ac.kr>
2017-12-26 00:39:39 +0900

    results in dictionaries indicating levels //// FileProcessor // process_nb_file() : added argument b_write_file // process_nb_node() : return result from CellListProcessor.process_cells() // //// CellListProcessor // added process_cells() : make an instance of the CellProcessor class & call for each cell // //// CellProcessor // added has_field() and implemented has_output() && has_source() using it // added has_symbol() : return dictionary of line numbers and source code line // process_cell() : just calls has_symbol() (maybe better as a virtual method) ////
    symbol_lines_in_file() : use FileProcessor.process_nb_file()

--- test_nbutils.py
+++ test_nbutils.py
@@ -21,50 +21,72 @@ class TestNButils(unittest.TestCase):
         self.assertIn('nbformat_minor', result)
 
     def test_has_symbol(self):
+        self.maxDiff = None
+
         file = self.file_processor.read_file()
         cells = file['cells']
         result = []
+
+        cp = nbutils.CellProcessor()
+
         for k, cell in enumerate(cells):
-            cell_result = nbutils.has_symbol(cell)
+            cp.set_cell(cell)
+            cell_result = cp.has_symbol()
             if cell_result:
                 result.append((k, cell_result))
 
         # compare with an expected list
-        expected = [(8, [(0, "L_AB_m = sy.symbols('L_AB_m', real=True, nonnegative=True)")]),
-                    (10, [(0, "w0_N_m = sy.symbols('w0_N_m', real=True)")]),
-                    (12, [(0, "E_Pa, I_m4 = sy.symbols('E_Pa, I_m4', positive=True)")]),
-                    (14, [(0, "x_m = sy.symbols('x_m', nonnegative=True)")]),
-                    (16, [(0, "R_A_N, M_A_Nm, R_B_N = sy.symbols('R_A_N, M_A_Nm, R_B_N', real=True)")])]
+        expected = [(8, [{'line number': 0, 'source': "L_AB_m = sy.symbols('L_AB_m', real=True, nonnegative=True)"}]),
+                    (10, [{'line number': 0, 'source': "w0_N_m = sy.symbols('w0_N_m', real=True)"}]),
+                    (12, [{'line number': 0, 'source': "E_Pa, I_m4 = sy.symbols('E_Pa, I_m4', positive=True)"}]),
+                    (14, [{'line number': 0, 'source': "x_m = sy.symbols('x_m', nonnegative=True)"}]),
+                    (16, [{'line number': 0,
+                           'source': "R_A_N, M_A_Nm, R_B_N = sy.symbols('R_A_N, M_A_Nm, R_B_N', real=True)"}])]
 
         self.assertSequenceEqual(expected, result)
 
     def test_cells_with_symbol(self):
+        self.maxDiff = None
         result = nbutils.symbol_lines_in_file(self.input_file_name)
 
         # compare with an expected list
-        expected = [(8, [(0, "L_AB_m = sy.symbols('L_AB_m', real=True, nonnegative=True)")]),
-                    (10, [(0, "w0_N_m = sy.symbols('w0_N_m', real=True)")]),
-                    (12, [(0, "E_Pa, I_m4 = sy.symbols('E_Pa, I_m4', positive=True)")]),
-                    (14, [(0, "x_m = sy.symbols('x_m', nonnegative=True)")]),
-                    (16, [(0, "R_A_N, M_A_Nm, R_B_N = sy.symbols('R_A_N, M_A_Nm, R_B_N', real=True)")])]
+        expected_result = [{'cell number': 8, 'result': [
+            {'line number': 0, 'source': "L_AB_m = sy.symbols('L_AB_m', real=True, nonnegative=True)"}]},
+                           {'cell number': 10,
+                            'result': [{'line number': 0, 'source': "w0_N_m = sy.symbols('w0_N_m', real=True)"}]},
+                           {'cell number': 12, 'result': [
+                               {'line number': 0, 'source': "E_Pa, I_m4 = sy.symbols('E_Pa, I_m4', positive=True)"}]},
+                           {'cell number': 14,
+                            'result': [{'line number': 0, 'source': "x_m = sy.symbols('x_m', nonnegative=True)"}]},
+                           {'cell number': 16, 'result': [{'line number': 0,
+                                                           'source': "R_A_N, M_A_Nm, R_B_N = sy.symbols('R_A_N, M_A_Nm, R_B_N', real=True)"}]}]
+        expected = {'file name': self.input_file_name,
+                    'result': expected_result}
 
-        self.assertSequenceEqual(expected, result)
+        self.assertDictEqual(expected, result)
 
     def test_replace_symbol(self):
+        self.maxDiff = None
         file = self.file_processor.read_file()
         cells = file['cells']
         result = []
+
+        cp = nbutils.CellProcessor()
+
         for k, cell in enumerate(cells):
-            cell_result = nbutils.has_symbol(cell)
+            cp.set_cell(cell)
+            cell_result = cp.has_symbol()
             if cell_result:
                 result.append((k, cell_result))
 
         # compare with an expected list
-        expected = [(8, [(0, "L_AB_m = sy.symbols('L_{AB}[m]', real=True, nonnegative=True)")]),
-                    (10, [(0, "w0_N_m = sy.symbols('w0[N/m]', real=True)")]),
-                    (12, [(0, "E_Pa, I_m4 = sy.symbols('E[Pa], I[m^{4}]', positive=True)")]),
-                    (14, [(0, "x_m = sy.symbols('x[m]', nonnegative=True)")]),
-                    (16, [(0, "R_A_N, M_A_Nm, R_B_N = sy.symbols('R_{A}[N], M_{A}[Nm], R_{B}[N]', real=True)")])]
+        expected = [
+            (8, [{'line number': 0, 'source': "L_AB_m = sy.symbols('L_{AB}[m]', real=True, nonnegative=True)"}]),
+            (10, [{'line number': 0, 'source': "w0_N_m = sy.symbols('w0[N/m]', real=True)"}]),
+            (12, [{'line number': 0, 'source': "E_Pa, I_m4 = sy.symbols('E[Pa], I[m^{4}]', positive=True)"}]),
+            (14, [{'line number': 0, 'source': "x_m = sy.symbols('x[m]', nonnegative=True)"}]),
+            (16, [{'line number': 0,
+                   'source': "R_A_N, M_A_Nm, R_B_N = sy.symbols('R_{A}[N], M_{A}[Nm], R_{B}[N]', real=True)"}])]
 
         self.assertSequenceEqual(expected, result)
 
