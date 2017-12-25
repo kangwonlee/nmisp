3e978a064fa3353d6017469af3d5e62c70b32843
KangWon Lee <kangwon.lee@kpu.ac.kr>
2017-12-26 01:06:15 +0900

    started SymbolConverter as a subclass of CellProcessorBase

--- symbol_converter.py
+++ symbol_converter.py
@@ -1,6 +1,11 @@
 import nb_file_util as fu
 
 
+class SymbolConverter(fu.CellProcessorBase):
+    def process_cell(self):
+        return self.has_symbol()
+
+
 def symbol_lines_in_file(input_file_name):
     file_processor = fu.FileProcessor(input_file_name)
     result = file_processor.process_nb_file()
