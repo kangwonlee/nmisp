4ddfd34b9cae629bd0e94ae099c505c7eca9a82a
KangWon Lee <kangwon.lee@kpu.ac.kr>
2017-12-26 12:41:44 +0900

    added more test cases

--- test_symbol_converter.py
+++ test_symbol_converter.py
@@ -55,29 +55,6 @@ class TestSymbolLister(unittest.TestCase):
 
         self.assertSequenceEqual(expected, result)
 
-    def test_replace_symbol(self):
-        self.maxDiff = None
-        file = self.file_processor.read_file()
-        cells = file['cells']
-        result = []
-
-        for k, cell in enumerate(cells):
-            self.cp.set_cell(cell)
-            cell_result = self.cp.has_symbol()
-            if cell_result:
-                result.append((k, cell_result))
-
-        # compare with an expected list
-        expected = [
-            (8, [{'line number': 0, 'source': "L_AB_m = sy.symbols('L_{AB}[m]', real=True, nonnegative=True)"}]),
-            (10, [{'line number': 0, 'source': "w0_N_m = sy.symbols('w0[N/m]', real=True)"}]),
-            (12, [{'line number': 0, 'source': "E_Pa, I_m4 = sy.symbols('E[Pa], I[m^{4}]', positive=True)"}]),
-            (14, [{'line number': 0, 'source': "x_m = sy.symbols('x[m]', nonnegative=True)"}]),
-            (16, [{'line number': 0,
-                   'source': "R_A_N, M_A_Nm, R_B_N = sy.symbols('R_{A}[N], M_{A}[Nm], R_{B}[N]', real=True)"}])]
-
-        self.assertSequenceEqual(expected, result)
-
 
 class MyLineConverterTesterBase(unittest.TestCase):
     def setUp(self):
@@ -250,5 +227,23 @@ class TestSymbolConverter00(MyLineConverterTesterBase):
                                 "w0_N_m = sy.Symbol('w0[N/m]', real=True)")
 
     def test_process_line_11(self):
-        self.check_process_line("w0_N_m = sy.Symbol('w0_N_m', real=True)",
-                                "w0_N_m = sy.Symbol('w0[N/m]', real=True)")
+        self.check_process_line("w0_N = sy.Symbol('w0_N_m', real=True)",
+                                "w0_N = sy.Symbol('w0[N/m]', real=True)")
+
+
+class TestSymbolConverter01(MyLineConverterTesterBase):
+    def test_process_line_00(self):
+        self.check_process_line("E_Pa, I_m4 = sy.symbols('E_Pa, I_m4', positive=True)",
+                                "E_Pa, I_m4 = sy.symbols('E[Pa], I[m^{4}]', positive=True)")
+
+    def test_process_line_01(self):
+        self.check_process_line("E, I = sy.symbols('E_Pa, I_m4', positive=True)",
+                                "E, I = sy.symbols('E[Pa], I[m^{4}]', positive=True)")
+
+    def test_process_line_10(self):
+        self.check_process_line("x_m = sy.symbols('x_m', nonnegative=True)",
+                                "x_m = sy.symbols('x[m]', nonnegative=True)")
+
+    def test_process_line_20(self):
+        self.check_process_line("R_A_N, M_A_Nm, R_B_N = sy.symbols('R_A_N, M_A_Nm, R_B_N', real=True)",
+                                "R_A_N, M_A_Nm, R_B_N = sy.symbols('R_{A}[N], M_{A}[Nm], R_{B}[N]', real=True)")
