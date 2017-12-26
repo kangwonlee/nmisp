153a039f797b7ac4603da232cea2b08935916191
KangWon Lee <kangwon.lee@kpu.ac.kr>
2017-12-26 13:17:27 +0900

    process_symbol_name(): default result = {symbol_name:symbol_name} : if symbol name does not have unit fromt the beginning

--- symbol_converter.py
+++ symbol_converter.py
@@ -121,9 +121,10 @@ class SymbolConverter(SymbolLister):
         return converted_source_line
 
     def process_symbol_name(self, symbol_name):
+        result = {symbol_name:symbol_name}
         wrapped = self.wrap_symbol_name(symbol_name)
         # first conversion layer : for majority of cases
-        result = self.apply_lookup_table(wrapped, symbol_name)
+        result.update(self.apply_lookup_table(wrapped, symbol_name))
         # second conversion layer : for N/m, N/m^{2} cases
         result.update(self.apply_lookup_table(result[symbol_name], symbol_name, self.secondary_table_dict))
         return result[symbol_name]
