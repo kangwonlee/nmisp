fc72f8de079eb8926905d7f307a378ea01a7e613
Kang Won LEE <kangwon.lee@kpu.ac.kr>
2018-08-12 12:03:09 +0900

    Added py.test
    Reused https://github.com/kangwonlee/17sol_mech_2/blob/master/tests/test_nb.py
    ref : Francesco Mosconi, Travis + Anaconda + Jupyter, https://github.com/ghego/travis_anaconda_jupyter

---
+++ test_nb.py
@@ -0,0 +1,119 @@
+# ref : Francesco Mosconi, Travis + Anaconda + Jupyter, https://github.com/ghego/travis_anaconda_jupyter
+
+
+import os
+import subprocess
+import tempfile
+
+
+def check_kernel_spec():
+    # https://jupyter-client.readthedocs.io/en/latest/api/kernelspec.html
+    import jupyter_client.kernelspec as jk
+    kernel_spec_manager = jk.KernelSpecManager()
+
+    print(kernel_spec_manager.get_all_specs())
+
+
+def _exec_notebook(path):
+    # http://nbconvert.readthedocs.io/en/latest/execute_api.html
+    # ijstokes et al, Command line execution of a jupyter notebook fails in default Anaconda 4.1, https://github.com/Anaconda-Platform/nb_conda_kernels/issues/34
+    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
+        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+                "--ExecutePreprocessor.timeout=1000",
+                "--ExecutePreprocessor.kernel_name=python",
+                "--output", fout.name, path]
+        subprocess.check_call(args)
+
+
+folder_list = (
+    '00_introduction', 
+    '01_root_finding', 
+    '02_num_int', 
+    '03_linear_algebra_1', 
+    '04_ode', 
+    '05_linear_algebra_2',
+)
+
+
+def test02():
+    path = os.path.join(os.pardir, folder_list[0])
+    ext = 'ipynb'
+
+    # recursive loop
+    for root, dirnames, filenames in os.walk(path):
+        if 'ipynb_checkpoints' not in root:
+            # files loop
+            for filename in filenames:
+                if os.path.splitext(filename)[-1].endswith(ext):
+                    print('test() : %s %s' % (root, filename))
+                    _exec_notebook(os.path.join(root, filename))
+
+
+def test03():
+    path = os.path.join(os.pardir, folder_list[1])
+    ext = 'ipynb'
+
+    # recursive loop
+    for root, dirnames, filenames in os.walk(path):
+        if 'ipynb_checkpoints' not in root:
+            # files loop
+            for filename in filenames:
+                if os.path.splitext(filename)[-1].endswith(ext):
+                    print('test() : %s %s' % (root, filename))
+                    _exec_notebook(os.path.join(root, filename))
+
+
+def test04():
+    path = os.path.join(os.pardir, folder_list[2])
+    ext = 'ipynb'
+
+    # recursive loop
+    for root, dirnames, filenames in os.walk(path):
+        if 'ipynb_checkpoints' not in root:
+            # files loop
+            for filename in filenames:
+                if os.path.splitext(filename)[-1].endswith(ext):
+                    print('test() : %s %s' % (root, filename))
+                    _exec_notebook(os.path.join(root, filename))
+
+
+def test05():
+    path = os.path.join(os.pardir, folder_list[3])
+    ext = 'ipynb'
+
+    # recursive loop
+    for root, dirnames, filenames in os.walk(path):
+        if 'ipynb_checkpoints' not in root:
+            # files loop
+            for filename in filenames:
+                if os.path.splitext(filename)[-1].endswith(ext):
+                    print('test() : %s %s' % (root, filename))
+                    _exec_notebook(os.path.join(root, filename))
+
+
+def test06():
+    path = os.path.join(os.pardir, folder_list[4])
+    ext = 'ipynb'
+
+    # recursive loop
+    for root, dirnames, filenames in os.walk(path):
+        if 'ipynb_checkpoints' not in root:
+            # files loop
+            for filename in filenames:
+                if os.path.splitext(filename)[-1].endswith(ext):
+                    print('test() : %s %s' % (root, filename))
+                    _exec_notebook(os.path.join(root, filename))
+
+
+def test07():
+    path = os.path.join(os.pardir, folder_list[5])
+    ext = 'ipynb'
+
+    # recursive loop
+    for root, dirnames, filenames in os.walk(path):
+        if 'ipynb_checkpoints' not in root:
+            # files loop
+            for filename in filenames:
+                if os.path.splitext(filename)[-1].endswith(ext):
+                    print('test() : %s %s' % (root, filename))
+                    _exec_notebook(os.path.join(root, filename))
