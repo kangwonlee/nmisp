06e4cbca4d21bfe93c1b4c2dabc18500da97e21a
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2018-10-26 08:50:57 -0700

    adding comments

--- build_util.py
+++ build_util.py
@@ -5,6 +5,7 @@ import subprocess
 import IPython
 
 
+# obtain ipython
 try:
     ipython = IPython.get_ipython()
 except AttributeError:
@@ -17,10 +18,18 @@ except AttributeError:
 
 
 def write_file(filename, code):
+    """
+    Write a new file
+    To make C/C++ source code file
+    """
     ipython.run_cell_magic("writefile", filename, code)
 
 
 def build_cpp(filename):
+    """
+    Build cpp file
+    filename : ex) test or test.cpp
+    """
     # Detect OS type because OSX may need different options
     # https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux/18790824
     
@@ -30,6 +39,7 @@ def build_cpp(filename):
 
     ipython.run_cell_magic(
             "bash", "", ""
+            # detect system type
             'unameOut="$(uname -s)"\n'
             'case "${unameOut}" in\n'
             '   Linux*)     machine=Linux;;\n'
@@ -38,16 +48,17 @@ def build_cpp(filename):
             '   MINGW*)     machine=MinGw;;\n'
             '   *)          machine="UNKNOWN:${unameOut}"\n'
             'esac\n'
+            # build command for system type
             'if [ $machine == "Linux" ]; then\n'
-            '    # build command for Linux\n'
+                 # build command for Linux
             f'    g++ -Wall -g -std=c++14 {filename} -o ./{basename} -Wa,-adhln={basename}.s\n'
             'elif [ "Mac" == $machine ]; then\n'
-            '    # build command for OSX\n'
-            '    # https://stackoverflow.com/questions/10990018/\n'
+                 # build command for OSX
+                 # https://stackoverflow.com/questions/10990018/
             f'    clang++ -S -mllvm --x86-asm-syntax=intel {filename}\n'
             f'    clang++ -Wall -g -std=c++14 {filename} -o ./{basename}\n'
             'else\n'
-            '    # Otherwise\n'
+                 # Otherwise
             f'    g++ -Wall -g -std=c++14 {filename} -o ./{basename}.s -S\n'
             f'    g++ -Wall -g -std=c++14 {filename} -o ./{basename}\n'
             'fi\n'
@@ -55,6 +66,10 @@ def build_cpp(filename):
 
 
 def run(cpp_filename):
+    """
+    Build cpp file
+    cpp_filename : ex) test or test.cpp
+    """
     basename, ext = os.path.splitext(cpp_filename)
     # https://stackoverflow.com/questions/4760215/running-shell-command-from-python-and-capturing-the-output
     # https://stackoverflow.com/questions/35160256/how-do-i-output-lists-as-a-table-in-jupyter-notebook
@@ -66,14 +81,21 @@ def run(cpp_filename):
 
 
 def cleanup(cpp_filename):
+    """
+    Build cpp file
+    cpp_filename : ex) test or test.cpp
+    """
     basename, ext = os.path.splitext(cpp_filename)
     if not ext:
         cpp_filename += '.cpp'
 
+    # to delete execution file
     if os.path.exists(basename):
         os.remove(basename)
     else:
         print(f"Unable to find {basename}")
         print(os.listdir())
+
+    # to delete source file
     os.remove(cpp_filename)
 
