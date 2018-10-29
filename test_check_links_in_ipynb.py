080cb1c3846b8a76ce40413206fa144c926e3ac4
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2018-10-29 02:20:47 -0700

    Added more patterns

--- test_check_links_in_ipynb.py
+++ test_check_links_in_ipynb.py
@@ -14,7 +14,7 @@ ipynb_file_list = [filename for filename in os.listdir(base_path) if filename.en
 
 
 def test_check_links_in_ipynb():
-    cli.check_links_in_ipynb(os.path.join(base_path, '00.ipynb'))
+    cli.check_links_in_ipynb(os.path.join(base_path, '10.ipynb'))
 
 patterns = [
     {
@@ -42,7 +42,57 @@ patterns = [
         'urls': {
             'https://stackoverflow.com/questions/8305866/how-to-analyze-a-programs-core-dump-file-with-gdb',
         }
-    }
+    },
+    {
+        'text':  '* Following code block is an [example](https://stackoverflow.com/questions/35656604/running-cython-in-jupyter-ipython) of the cython code.' ,
+        'urls': {
+            'https://stackoverflow.com/questions/35656604/running-cython-in-jupyter-ipython',
+        }
+    },
+    {
+        'text':  '* Here `%%cython` command is doing all the [plumbing](http://blog.yclin.me/gsoc/2016/07/23/Cython-IPython/) for us.\n* Under the hood, [more](https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html) is going on as we may see later.' ,
+        'urls': {
+            'http://blog.yclin.me/gsoc/2016/07/23/Cython-IPython/',
+            'https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html',
+        }
+    },
+    {
+        'text':  ('* Cython can call C/C++ functions as follows.\n'
+                '[[ref0](https://cython.readthedocs.io/en/latest/src/userguide/external_C_code.html)]\n'
+                ', [[ref1](https://stackoverflow.com/questions/37426534/how-can-i-import-an-external-c-function-into-an-ipython-notebook-using-cython)]\n'
+                ', [[ref2](https://stackoverflow.com/questions/19260253/cython-compiling-error-multiple-definition-of-functions)]\n'
+                ', [[ref3](https://media.readthedocs.org/pdf/cython/stable/cython.pdf)]\n'
+                ', [[ref4](http://www.scipy-lectures.org/advanced/interfacing_with_c/interfacing_with_c.html)]\n'
+                ' \n'
+                ' '
+        ) ,
+        'urls': {
+            'https://cython.readthedocs.io/en/latest/src/userguide/external_C_code.html',
+            'https://stackoverflow.com/questions/37426534/how-can-i-import-an-external-c-function-into-an-ipython-notebook-using-cython',
+            'https://stackoverflow.com/questions/19260253/cython-compiling-error-multiple-definition-of-functions',
+            'https://media.readthedocs.org/pdf/cython/stable/cython.pdf',
+            'http://www.scipy-lectures.org/advanced/interfacing_with_c/interfacing_with_c.html',
+        }
+    },
+    {
+        'text':  '* Nowadays in late 2010s, there is an open source project of [**python-control**](https://github.com/python-control/).' ,
+        'urls': {
+            'https://github.com/python-control/',
+        }
+    },
+    {
+        'text':  '* One of the challenges is a numerical library named [**Slycot**](https://github.com/python-control/Slycot) mostly in Fortran.' ,
+        'urls': {
+            'https://github.com/python-control/Slycot',
+        }
+    },
+    {
+        'text':  '* Please refer to [**python-control**](https://github.com/python-control/python-control) and [**Slycot**](https://github.com/python-control/Slycot) for more information.' ,
+        'urls': {
+            'https://github.com/python-control/python-control',
+            'https://github.com/python-control/Slycot',
+        }
+    },
 ]
 
 
