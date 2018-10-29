bb5fa657f3e1f36c34d2bc668202a76af63b5bdd
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2018-10-29 02:07:37 -0700

    Added get_re_markdown_simple_link()

--- check_links_in_ipynb.py
+++ check_links_in_ipynb.py
@@ -7,6 +7,10 @@ def is_cell_markdown(cell):
     return cell['cell_type'].strip().lower().startswith('markdown')
 
 
+def get_re_markdown_simple_link():
+    return re.compile(r'\[.+?\]\((.+?)\)')
+
+
 def check_links_in_ipynb(filename):
     with open(filename, encoding='utf-8') as ipynb:
         nb = nbformat.read(ipynb, nbformat.NO_CONVERT)
