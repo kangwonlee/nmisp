7e920c4840c85b863589ffed7cefd01c9d64d975
Kang Won LEE <kangwon.lee@kpu.ac.kr>
2018-12-01 20:47:53 +0900

    get_exec_notebook() : run_this_dict -> os_to_function_table

--- test_nb.py
+++ test_nb.py
@@ -87,13 +87,12 @@ def _exec_notebook_win(path):
 # https://docs.pytest.org/en/latest/fixture.html#scope-sharing-a-fixture-instance-across-tests-in-a-class-module-or-session
 def get_exec_notebook():
     # https://docs.python.org/3/library/platform.html#cross-platform
-    run_this_dict = {
+    os_to_function_table = {
         'posix': _exec_notebook_nix,
         'nt': _exec_notebook_win,
     }
 
-    _exec_notebook = run_this_dict.get(os.name, _exec_notebook_nix)
-    return _exec_notebook
+    return os_to_function_table.get(os.name, _exec_notebook_nix)
 
 
 def make_file_list(path=os.path.abspath(os.path.join(os.path.split(__file__)[0], os.pardir)), ext='ipynb'):
