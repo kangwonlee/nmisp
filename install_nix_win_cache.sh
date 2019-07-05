662fe4c6a640452476abf8c5ef11e289ea24ba5a
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-07-05 21:44:28 +0900

    check if miniconda path exists

--- install_nix_win_cache.sh
+++ install_nix_win_cache.sh
@@ -18,6 +18,14 @@ elif  [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
     fi;
 fi;
 export PATH="$MINICONDA_PATH:$MINICONDA_SUB_PATH:$MINICONDA_LIB_BIN_PATH:$PATH";
+# begin checking miniconda existance
+echo "checking if folder $MINICONDA_SUB_PATH exists"
+if [[ -d $MINICONDA_SUB_PATH ]]; then
+    echo "folder $MINICONDA_SUB_PATH exists"
+else
+    echo "folder $MINICONDA_SUB_PATH does not exist"
+fi;
+# end checking miniconda existance
 source $MINICONDA_PATH/etc/profile.d/conda.sh;
 hash -r;
 echo $TRAVIS_OS_NAME
