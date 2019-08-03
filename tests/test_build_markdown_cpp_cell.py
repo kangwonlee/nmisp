3141bce35a68ae1435265d6cdac9470af9b35eda
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-08-04 01:17:35 +0900

    line breaks in c++ code (cont)

--- tests/test_build_markdown_cpp_cell.py
+++ tests/test_build_markdown_cpp_cell.py
@@ -133,7 +133,38 @@ cpp_test_cases = [
     '\n'
     )},
 {'result': 0, 'cpp_filename': 'account_module.cpp'}],
-[{'cell_type': 'markdown', 'metadata': {}, 'source': '\n``` C++\n// Begin account_module_user.cpp\n#include <iostream>\n#include <cstdint>\n\n#include "account_module.h"\n\nusing namespace std;\n\nint32_t main(int32_t argn, char ** argv){\n\n    cout << "account_module.check() = "<< account::check() <<\'\\n\';\n\n    account::deposit(10000);\n\n    cout << "account_module.check() = "<< account::check() <<\'\\n\';\n\n    account::withdraw(3000);\n\n    cout << "account_module.check() = "<< account::check() <<\'\\n\';\n\n    account::deposit(5000);\n\n    cout << "account_module.check() = "<< account::check() <<\'\\n\';\n\n    return 0;\n}\n// End account_module_user.cpp\n// Build command : g++ -Wall -g account_module.cpp account_module_user.cpp -o account_module_user\n```\n\n'},
+[{'cell_type': 'markdown', 'metadata': {}, 'source': (
+    '``` C++\n'
+    '// Begin account_module_user.cpp\n'
+    '#include <iostream>\n'
+    '#include <cstdint>\n'
+    '\n'
+    '#include "account_module.h"\n'
+    '\n'
+    'using namespace std;\n'
+    '\n'
+    'int32_t main(int32_t argn, char ** argv){\n'
+    '\n'
+    '    cout << "account_module.check() = "<< account::check() <<\'\\n\';\n'
+    '\n'
+    '    account::deposit(10000);\n'
+    '\n'
+    '    cout << "account_module.check() = "<< account::check() <<\'\\n\';\n'
+    '\n'
+    '    account::withdraw(3000);\n'
+    '\n'
+    '    cout << "account_module.check() = "<< account::check() <<\'\\n\';\n'
+    '\n'
+    '    account::deposit(5000);\n'
+    '\n'
+    '    cout << "account_module.check() = "<< account::check() <<\'\\n\';\n'
+    '\n'
+    '    return 0;\n'
+    '}\n'
+    '// End account_module_user.cpp\n'
+    '// Build command : g++ -Wall -g account_module.cpp account_module_user.cpp -o account_module_user\n'
+    '```\n'
+    '\n')},
 {'result': 0, 'cpp_filename': 'account_module_user.cpp'}],
 [{'cell_type': 'markdown', 'metadata': {}, 'source': '``` C++\n// Begin account_module.h\n#include <cstdint>\n#include <iostream>\n\nint32_t balance;\n\n\nvoid deposit (int32_t amount){\n    std::cout << "Deposit " << amount << \'\\n\';\n    std::cout << "to " << & balance << \'\\n\';\n    balance += amount;\n}\n\n\nvoid withdraw (int32_t amount){\n    std::cout << "Withdraws " << amount << \'\\n\';\n    std::cout << "from " << & balance << \'\\n\';\n    balance += -amount;\n}\n\n\nint32_t check(){\n    return balance;\n}\n// End account_module.h\n\n```'},
 {'result': 0, 'cpp_filename': 'account_module.h'}],
