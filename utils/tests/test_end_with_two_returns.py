9781a2f0088721f180362c99197081cd421dccf5
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-08-18 16:12:36 +0900

    added test_gen_cells()

--- utils/tests/test_end_with_two_returns.py
+++ utils/tests/test_end_with_two_returns.py
@@ -2,6 +2,8 @@ import os
 import sys
 import unittest
 
+import nbformat
+
 
 sys.path.insert(0,
     os.path.abspath(
@@ -13,3 +15,25 @@ sys.path.insert(0,
 
 
 import end_with_two_returns as tr
+
+
+class TestFile(unittest.TestCase):
+    def setUp(self):
+        self.fp = open(
+            os.path.join(
+                os.path.dirname(__file__), 
+                'sample.ipynb'
+            ), 
+            encoding = 'utf-8'
+        )
+    
+    def tearDown(self):
+        self.fp.close()
+
+    def test_gen_cells(self):
+        for cell in tr.gen_cells(self.fp):
+            self.assertIsInstance(cell, nbformat.notebooknode.NotebookNode)
+
+
+if "__main__" == __name__:
+    unittest.main()
