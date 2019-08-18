b14f52b510a13a2d17aca6d0ace8a73da0e700df
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-08-18 15:30:08 +0900

    added get_par_dir()

--- utils/end_with_two_returns.py
+++ utils/end_with_two_returns.py
@@ -5,3 +5,7 @@ import nbformat
 
 def get_cur_dir():
     return os.path.abspath(os.path.basename(__file__))
+
+
+def get_par_dir():
+    return os.path.abspath(os.path.join(os.path.basename(__file__), os.pardir))
