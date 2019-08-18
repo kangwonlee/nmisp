f82e9b6e149dfd03d11ee0a133bcd722f4eba923
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-08-18 15:36:48 +0900

    added gen_items()

--- utils/end_with_two_returns.py
+++ utils/end_with_two_returns.py
@@ -11,6 +11,13 @@ def get_par_dir():
     return os.path.abspath(os.path.join(os.path.basename(__file__), os.pardir))
 
 
+def gen_items(path):
+    for item in os.listdir(path):
+        path_item = os.path.abspath(os.path.join(path, item))
+        assert os.path.exists(path_item), f"Missing : {path_item}"
+        yield path_item
+
+
 def gen_folders(path=get_par_dir()):
     for item in os.listdir(path):
         path_item = os.path.abspath(os.path.join(path, item))
