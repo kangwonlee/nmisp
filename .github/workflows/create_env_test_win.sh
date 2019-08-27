111a40d90b882ad27f005542a4151b1e5c48d23a
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-08-28 03:35:01 +0900

    two steps for win

--- .github/workflows/create_env_test_win.sh
+++ .github/workflows/create_env_test_win.sh
@@ -1,43 +1,6 @@
 echo "============================================"
-echo "Setting environment variables"
+echo "PATH = $PATH"
 echo "============================================"
-if [ $RUNNER_WORKSPACE ]; then
-    export MINICONDA_PATH=`cygpath --unix $RUNNER_WORKSPACE`/miniconda
-else
-    export MINICONDA_PATH=../miniconda
-fi
-
-echo "MINICONDA_PATH = $MINICONDA_PATH"
-export MINICONDA_SUB_PATH=$MINICONDA_PATH/Script
-echo "MINICONDA_SUB_PATH = $MINICONDA_SUB_PATH"
-
-export MINICONDA_PATH_WIN=`cygpath --windows $MINICONDA_PATH`;
-echo "MINICONDA_PATH_WIN = $MINICONDA_PATH_WIN"
-
-export MINICONDA_LIB_BIN_PATH=$MINICONDA_PATH/Library/bin
-echo "MINICONDA_LIB_BIN_PATH = $MINICONDA_LIB_BIN_PATH"
-
-export MINICONDA_DOWNLOAD=$MINICONDA_PATH/download
-echo "MINICONDA_DOWNLOAD = $MINICONDA_DOWNLOAD"
-
-echo "============================================"
-echo "Install openssl for Windows"
-echo "============================================"
-choco install openssl.light;
-
-echo "============================================"
-echo "Downloading and Installing Miniconda"
-echo "============================================"
-
-choco install miniconda3 --params="'/JustMe /AddToPath:1 /D:$MINICONDA_PATH_WIN'";
-
-echo "============================================"
-echo "Finished Installing Miniconda"
-echo "============================================"
-
-echo "exporting a new path ======================="
-export PATH="$MINICONDA_PATH:$MINICONDA_SUB_PATH:$MINICONDA_LIB_BIN_PATH:$MINICONDA_PATH_WIN:$PATH"
-echo $PATH
 echo "init conda ================================="
 conda init $SHELL
 echo "pwd ========================================"
