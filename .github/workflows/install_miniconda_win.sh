b9626c17bc2bf5ab014bc6b3f488f89ae2de33a9
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-08-28 09:47:14 +0900

    check path exists

--- .github/workflows/install_miniconda_win.sh
+++ .github/workflows/install_miniconda_win.sh
@@ -1,3 +1,10 @@
+exists() {
+    folder = $1
+    if [ -d $folder ]; then
+        echo "folder $folder exists"
+    fi
+}
+
 echo "============================================"
 echo "Setting environment variables"
 echo "============================================"
@@ -6,16 +13,20 @@ if [ $CONDA ]; then
 else
     export MINICONDA_PATH=../miniconda
 fi
-
 echo "MINICONDA_PATH = $MINICONDA_PATH"
+exists $MINICONDA_PATH
+
 export MINICONDA_SUB_PATH=$MINICONDA_PATH/Script
 echo "MINICONDA_SUB_PATH = $MINICONDA_SUB_PATH"
+exists $MINICONDA_SUB_PATH
 
 export MINICONDA_PATH_WIN=`cygpath --windows $MINICONDA_PATH`;
 echo "MINICONDA_PATH_WIN = $MINICONDA_PATH_WIN"
+exists $MINICONDA_PATH_WIN
 
 export MINICONDA_LIB_BIN_PATH=$MINICONDA_PATH/Library/bin
 echo "MINICONDA_LIB_BIN_PATH = $MINICONDA_LIB_BIN_PATH"
+exists $MINICONDA_LIB_BIN_PATH
 
 export MINICONDA_DOWNLOAD=$MINICONDA_PATH/download
 echo "MINICONDA_DOWNLOAD = $MINICONDA_DOWNLOAD"
