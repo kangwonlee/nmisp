e6e1cd22716e5ad2b6a817d7237183535f3d2f0b
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-08-28 10:36:01 +0900

    removed unused paths & exists()

--- .github/workflows/create_env_test_win.sh
+++ .github/workflows/create_env_test_win.sh
@@ -1,10 +1,3 @@
-exists() {
-    folder=$1
-    if [ -d $folder ]; then
-        echo "folder $folder exists"
-    fi
-}
-
 echo "============================================"
 echo "Setting environment variables"
 echo "============================================"
@@ -14,29 +7,12 @@ else
     export MINICONDA_PATH=../miniconda
 fi
 echo "MINICONDA_PATH = $MINICONDA_PATH"
-exists $MINICONDA_PATH
 
-if [ -d $MINICONDA_PATH/Scripts ]; then
-    export MINICONDA_SUB_PATH=$MINICONDA_PATH/Scripts
-elif [ -d $MINICONDA_PATH/bin ]; then
-    export MINICONDA_SUB_PATH=$MINICONDA_PATH/bin
-else
-    echo "ls $MINICONDA_PATH ========================="
-    ls $MINICONDA_PATH
-fi
+export MINICONDA_SUB_PATH=$MINICONDA_PATH/Scripts
 echo "MINICONDA_SUB_PATH = $MINICONDA_SUB_PATH"
-exists $MINICONDA_SUB_PATH
-
-export MINICONDA_PATH_WIN=`cygpath --windows $MINICONDA_PATH`;
-echo "MINICONDA_PATH_WIN = $MINICONDA_PATH_WIN"
-exists $MINICONDA_PATH_WIN
 
 export MINICONDA_LIB_BIN_PATH=$MINICONDA_PATH/Library/bin
 echo "MINICONDA_LIB_BIN_PATH = $MINICONDA_LIB_BIN_PATH"
-exists $MINICONDA_LIB_BIN_PATH
-
-export MINICONDA_DOWNLOAD=$MINICONDA_PATH/download
-echo "MINICONDA_DOWNLOAD = $MINICONDA_DOWNLOAD"
 
 echo "============================================"
 export PATH=$MINICONDA_PATH:$MINICONDA_SUB_PATH:$PATH
