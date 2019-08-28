4c4dc9bd781d9f9083495f094c87b0fe73448177
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-08-28 11:48:50 +0900

    calling build_env.sh

--- .github/workflows/create_env_test_osx.sh
+++ .github/workflows/create_env_test_osx.sh
@@ -32,28 +32,6 @@ echo "============================================"
 echo "Finished Installing Miniconda"
 echo "============================================"
 
-echo "exporting a new path ======================="
-export PATH="$MINICONDA_PATH:$MINICONDA_SUB_PATH:$PATH"
-echo "init conda ================================="
-conda init bash
-echo "~/$BASHRC =================================="
-. ~/$BASHRC
-echo "hash -r ===================================="
-hash -r
-echo "============================================"
-echo "CONDA_PYTHON = $CONDA_PYTHON ==============="
-echo "============================================"
-echo "checking python version ===================="
-python --version
-echo "updating conda ============================="
-conda config --set always_yes yes --set changeps1 no;
-conda update -q conda;
-echo "conda info -a =============================="
-conda info -a
-echo "create test-environment ===================="
-conda env create -n test-environment -f ./tests/environment.${CONDA_PYTHON}.yml
-echo "activate test-environment =================="
-conda activate test-environment
-conda list
+. ./.github/workflows/build_env.sh
 
 . ./.github/workflows/run_test.sh
--- .github/workflows/create_env_test_win.sh
+++ .github/workflows/create_env_test_win.sh
@@ -14,27 +14,6 @@ echo "MINICONDA_SUB_PATH = $MINICONDA_SUB_PATH"
 export MINICONDA_LIB_BIN_PATH=$MINICONDA_PATH/Library/bin
 echo "MINICONDA_LIB_BIN_PATH = $MINICONDA_LIB_BIN_PATH"
 
-export PATH=$MINICONDA_PATH:$MINICONDA_SUB_PATH:$PATH
-echo "init conda ================================="
-conda init bash
-echo "~/$BASHRC =================================="
-. ~/$BASHRC
-echo "hash -r ===================================="
-hash -r
-echo "============================================"
-echo "CONDA_PYTHON = $CONDA_PYTHON ==============="
-echo "============================================"
-echo "checking python version ===================="
-python --version
-echo "updating conda ============================="
-conda config --set always_yes yes --set changeps1 no;
-conda update -q conda;
-echo "conda info -a =============================="
-conda info -a
-echo "create test-environment ===================="
-conda env create -n test-environment -f ./tests/environment.${CONDA_PYTHON}.yml
-echo "activate test-environment =================="
-conda activate test-environment
-conda list
+. ./.github/workflows/build_env.sh
 
 . ./.github/workflows/run_test.sh
