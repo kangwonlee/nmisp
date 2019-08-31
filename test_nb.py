39510d92ef1816c1bc48fd502dfe0b8f647d2105
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-08-31 23:04:58 +0900

    Squashed 'tests/' changes from f10ab90..d09fdef
    d09fdef Merge branch 'test_ipynb/hotfix/test-ipynb-ignore-folder' into test_ipynb/master
    a613bc0 detect empty file list
    2a37f08 env_ignore_folder_2() : do not add TEST_IPYNB_IGNORE_FOLDER
    e430af3 added test_get_ignore_list()
    b540304 get_ignore_list() : add if TEST_IPYNB_IGNORE_FOLDER exists
    96de679 refactored get_ignore_list()
    5a53aae whitespace
    d8363cc added test_make_file_list_env_input()
    6c3c03e test_make_file_list() : is ignore() ?
    3ba7c69 test_make_file_list()
    acf5180 using env_ignore_folder_2
    31fe488 added fixture env_ignore_folder_2()
    3ee5364 added test_is_ignore_env_ignore_folder()
    8cc8eae is_ignore() : TEST_IPYNB_IGNORE_FOLDER
    c8e8660 no cache: added install_nix_win.sh
    8070937 no cache : before_install_nix_win.sh
    c809f1d test_ipynb_file() : check file exists
    0d435ef make_file_list() : use file_path
    a2b442c FileNotFoundError -> assert not os.path.exists()
    98d3eda test_is_ignore() : added false_00
    1cd4003 is_ignore() : using map()
    b97f661 make_file_list() : use is_ignore()
    05d4abf added is_ignore()
    2f92fa5 make_file_list() : check the file exists
    999e0a1 test_ipynb_file() : param use env TEST_IPYNB_FOLDER
    2671bd9 if path missing, use pardir
    f3bc7cd make_file_list() : missing path will raise an exeption
    
    git-subtree-dir: tests
    git-subtree-split: d09fdefbeb1ef55ea91861a0b4c727bac13c8691

--- test_nb.py
+++ test_nb.py
@@ -95,34 +95,137 @@ def get_exec_notebook():
     return os_to_function_table.get(os.name, _exec_notebook_nix)
 
 
-def make_file_list(path=os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)), ext='ipynb'):
+def test_is_ignore():
+
+    true_00 = os.sep.join('~/Documents/Python Scripts/proj/subfolder/.ipynb_checkpoints'.split('/'))
+    assert is_ignore(true_00)
+
+    false_00 = os.sep.join('~/Documents/Python Scripts/proj/subfolder'.split('/'))
+    assert not is_ignore(false_00)
+
+
+@pytest.fixture(scope="function")
+def env_ignore_folder_2():
+
+    if 'TEST_IPYNB_IGNORE_FOLDER' in os.environ:
+        backup_env = os.environ.get('TEST_IPYNB_IGNORE_FOLDER', '')
+    else:
+        backup_env = False
+
+    os.environ['TEST_IPYNB_IGNORE_FOLDER'] = os.pathsep.join(['subfolder', 'temp'])
+
+    yield os.environ
+
+    # restore env var
+    if isinstance(backup_env, os.PathLike):
+        os.environ['TEST_IPYNB_IGNORE_FOLDER'] = backup_env
+    else:
+        del os.environ['TEST_IPYNB_IGNORE_FOLDER']
+
+
+def test_is_ignore_env_ignore_folder(env_ignore_folder_2):
+    true_00 = os.sep.join('~/Documents/Python Scripts/proj/subfolder/.ipynb_checkpoints'.split('/'))
+    assert is_ignore(true_00)
+
+    true_01 = os.sep.join('~/Documents/Python Scripts/proj/subfolder'.split('/'))
+    assert is_ignore(true_01)
+
+    false_00 = os.sep.join('~/Documents/Python Scripts/proj/'.split('/'))
+    assert not is_ignore(false_00)
+
+
+def is_ignore(path):
+    path_list = path.split(os.sep)
+
+    ignore_list = get_ignore_list()
+
+    return any(map(lambda path_part: path_part in ignore_list, path_list))
+
+
+def test_get_ignore_list():
+    assert all(get_ignore_list())
+
+
+def get_ignore_list():
+    ignore_list = ['.ipynb_checkpoints', '.git', '__pycache__', '.pytest_cache']
+
+    if 'TEST_IPYNB_IGNORE_FOLDER' in os.environ:
+        ignore_list += os.environ['TEST_IPYNB_IGNORE_FOLDER'].split(os.pathsep)
+
+    assert all(map(lambda path: isinstance(path, str), ignore_list))
+
+    return ignore_list
+
+
+def make_file_list(path='', ext='ipynb'):
+
+    path = os.path.abspath(path)
+
+    if not os.path.exists(path):
+        path=os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
     file_list = []
 
     # recursive loop
     for root, _, filenames in os.walk(path):
-        if not (
-                ('.ipynb_checkpoints' in root)
-                or ('.git' in root)
-                or ('__pycache__'in root)
-                or ('.pytest_cache' in root)
-            ):
+        if not (is_ignore(root)):
             # files loop
             for filename in filenames:
                 if os.path.splitext(filename)[-1].endswith(ext):
-                    file_list.append(os.path.join(root, filename))
+                    file_path = os.path.join(root, filename)
+                    assert isinstance(file_path, str)
+                    assert os.path.exists(file_path), f"File Not Found {file_path}"
+                    file_list.append(file_path)
 
     return file_list
+
+
+def test_make_file_list(env_ignore_folder_2):
+
+    list_under_test = make_file_list()
+
+    assert list_under_test, "List empty"
+
+    for filename in list_under_test:
+        assert isinstance(filename, str)
+        assert os.path.exists(filename), f"File Not Found {filename}"
+        assert not is_ignore(filename), f"Unexpected file {filename}"
+
+
+def test_make_file_list_env_input(env_ignore_folder_2):
+    list_under_test = make_file_list(
+        os.path.abspath(
+            os.environ.get('TEST_IPYNB_FOLDER', '')
+        )
+    )
+
+    assert list_under_test, "List empty"
+
+    for filename in make_file_list(os.path.abspath(
+                os.environ.get('TEST_IPYNB_FOLDER', '')
+            )):
+        assert isinstance(filename, str)
+        assert os.path.exists(filename), f"File Not Found {filename}"
+        assert not is_ignore(filename), f"Unexpected file {filename}"
 
 
 # https://docs.pytest.org/en/latest/example/parametrize.html
 @pytest.mark.parametrize(
     "filename, _exec_notebook",
     itertools.zip_longest(
-        make_file_list(),
+        make_file_list(
+            os.path.abspath(
+                os.environ.get('TEST_IPYNB_FOLDER', '')
+            )
+        ),
         [get_exec_notebook()], fillvalue=get_exec_notebook()
     )
 )
 def test_ipynb_file(filename, _exec_notebook):
     print('test() : %s' % filename)
+    assert os.path.exists(filename), f"File Not Found {filename}"
     _exec_notebook(filename)
+
+
+if "__main__" == __name__:
+    pytest.main()
