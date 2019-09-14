bc1c797543144567a27851ff9623c26d61ce8c5e
KangWon LEE (nmisp) <kangwon.lee@kpu.ac.kr>
2019-09-15 00:10:38 +0900

    started eca homework 01

---
+++ 00_introduction/eca_hw_01.py
@@ -0,0 +1,30 @@
+# To add a new cell, type '#%%'
+# To add a new markdown cell, type '#%% [markdown]'
+#%%
+import pylab as py
+
+
+#%% [markdown]
+# # 공학 전산 해석 숙제 01
+# # Engineering Computational Analysis Homework 01
+#
+
+#%% [markdown]
+# ## 01 함수의 그래프
+# ## 01 Plot of a function
+# 
+
+#%% [markdown]
+# 공학 관련 함수를 하나 선정하여 파이썬 함수로 구현하시오.
+# Choose a function related to engineering and implement in python.
+# 
+
+#%% [markdown]
+# 독립 변수의 $x$의 적당한 범위에서 함수값을 `plot`으로 표시하시오.
+# Within an appropriate range of the independent variable, indicate the function valuse using `plot`.
+# 
+
+#%% [markdown]
+# 가로 세로 축 변수 이름과 단위를 명기하시오.
+# Explicitly indicate horizontal and vertical axis labels and units.
+# 
