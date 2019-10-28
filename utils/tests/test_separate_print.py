9b1f357834e2f54d841e4e2b69b8cdd6d4bf2c32
KangWon LEE (nmisp) <kangwon.lee@kpu.ac.kr>
2019-10-28 19:14:19 +0900

    sample text as fixture

--- utils/tests/test_separate_print.py
+++ utils/tests/test_separate_print.py
@@ -1,2 +1,31 @@
-def sample_cell():
-    return 'print(\'\'\'3.2.5.2\'\'\')\nf = sy.Function(\'f\')\nprint("f(x).diff(x, x) + f(x) = %s" % (f(x).diff(x, x) + f(x)))\nprint("sy.dsolve(f(x).diff(x, x) + f(x)) = %s" % sy.dsolve(f(x).diff(x, x) + f(x)))\n\nm, c, k, t = sy.symbols(\'m c k t\')\nx = sy.Function(\'x\')\nvib_eq = m * x(t).diff(t, t) + c * x(t).diff(t) + k * x(t)\nresult = sy.dsolve(vib_eq)\nprint("sy.dsolve() result")\nprint(result)\n\nforced_vib_eq = m * x(t).diff(t, t) + c * x(t).diff(t) + k * x(t) - sy.sin(t)\nresult = sy.dsolve(forced_vib_eq)\nprint("forced vibration result:")\nprint(result)\n\n'
+import os
+import sys
+
+import pytest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
+
+import separate_print as sp
+
+
+@pytest.fixture
+def sample_cell() -> str:
+    return (
+        'print(\'\'\'3.2.5.2\'\'\')\n'
+        'f = sy.Function(\'f\')\n'
+        'print("f(x).diff(x, x) + f(x) = %s" % (f(x).diff(x, x) + f(x)))\n'
+        'print("sy.dsolve(f(x).diff(x, x) + f(x)) = %s" % sy.dsolve(f(x).diff(x, x) + f(x)))\n'
+        '\n'
+        'm, c, k, t = sy.symbols(\'m c k t\')\n'
+        'x = sy.Function(\'x\')\n'
+        'vib_eq = m * x(t).diff(t, t) + c * x(t).diff(t) + k * x(t)\n'
+        'result = sy.dsolve(vib_eq)\n'
+        'print("sy.dsolve() result")\n'
+        'print(result)\n'
+        '\n'
+        'forced_vib_eq = m * x(t).diff(t, t) + c * x(t).diff(t) + k * x(t) - sy.sin(t)\n'
+        'result = sy.dsolve(forced_vib_eq)\n'
+        'print("forced vibration result:")\n'
+        'print(result)\n'
+        '\n'
+    )
