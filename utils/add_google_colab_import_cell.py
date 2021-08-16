bbabea0d9bf5c5e843744f78ed26a8f259dcaf89
KangWon LEE (nmisp) <kangwon.lee@kpu.ac.kr>
2021-08-16 16:50:49 +0900

    test code
    set path after argv len check

--- utils/add_google_colab_import_cell.py
+++ utils/add_google_colab_import_cell.py
@@ -1,15 +1,19 @@
 import os
 import sys
 
-
 sys.path.insert(0, os.path.dirname(__file__))
-
-
 import nb_file_util as nb
 
 
 def main(argv=sys.argv):
-    pass
+    if 2 <= len(argv):
+        path = argv[1]
+    else:
+        path = nb.get_upper_folder()
+
+    code = "# test code"
+
+    nb.add_code_to_all_ipynb_tree(0, code, path)
 
 
 if "__main__" == __name__:
