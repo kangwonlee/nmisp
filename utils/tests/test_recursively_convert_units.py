7bc1ce395a432ad6c2413ec445ef6f01e1d049d2
KangWon LEE <kangwon@gmail.com>
2023-04-09 16:10:18 +0900

    added false case

--- utils/tests/test_recursively_convert_units.py
+++ utils/tests/test_recursively_convert_units.py
@@ -12,6 +12,26 @@ sys.path.insert(
 import recursively_convert_units as rcu
 
 
-def test__recursively_convert_units__is_ignore():
-    sample_path = pathlib.Path(__file__).parent.absolute() / "sample.ipynb"
+@pytest.fixture
+def utils_test_folder() -> pathlib.Path:
+    return pathlib.Path(__file__).parent.absolute()
+
+
+@pytest.fixture
+def utils_folder(utils_test_folder:pathlib.Path) -> pathlib.Path:
+    return utils_test_folder.parent.absolute()
+
+
+@pytest.fixture
+def proj_folder(utils_folder:pathlib.Path) -> pathlib.Path:
+    return utils_folder.parent.absolute()
+
+
+def test__recursively_convert_units__is_ignore__true(utils_test_folder):
+    sample_path = utils_test_folder / "sample.ipynb"
     assert rcu.is_ignore(sample_path)
+
+
+def test__recursively_convert_units__is_ignore__false(proj_folder):
+    sample_path = proj_folder / "10_root_finding" / "10_sequential"
+    assert not rcu.is_ignore(sample_path)
