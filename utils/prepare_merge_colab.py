4475aeb7c98e1bc9b713a13d727200788fab396e
KangWon LEE <kangwon@gmail.com>
2023-04-09 14:36:44 +0900

    removed get_google_colab_import_cell() function

--- utils/prepare_merge_colab.py
+++ utils/prepare_merge_colab.py
@@ -23,20 +23,5 @@ def main(argv=sys.argv):
         nb.write_nodes_to_ipynb(full_path, nodes)
 
 
-def get_google_colab_import_cell(folder_name:str, repo_name:str="nmisp") -> str:
-    code = (
-        "# This cell is for the Google Colaboratory\n"
-        "# https://stackoverflow.com/a/63519730\n"
-        "if 'google.colab' in str(get_ipython()):\n"
-        "  # https://colab.research.google.com/notebooks/io.ipynb\n"
-        "  import google.colab.drive as gcdrive\n"
-        "  # may need to visit a link for the Google Colab authorization code\n"
-        '''  gcdrive.mount("/content/drive/")\n'''
-        "  import sys\n"
-        f'''  sys.path.insert(0,"/content/drive/My Drive/Colab Notebooks/{repo_name}/{folder_name}")\n'''
-    )
-    return code
-
-
 if "__main__" == __name__:
     main(sys.argv)
