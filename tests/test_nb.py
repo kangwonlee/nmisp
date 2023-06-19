65a676b7b077d7ef4487b5ab83a072f863f283f5
KangWon LEE <kangwon@gmail.com>
2023-06-19 09:34:43 +0900

    using pathlib

--- tests/test_nb.py
+++ tests/test_nb.py
@@ -1,24 +1,15 @@
 # ref : Francesco Mosconi, Travis + Anaconda + Jupyter, https://github.com/ghego/travis_anaconda_jupyter
 
-
-import itertools
 import os
+import pathlib
 import subprocess
-import tempfile
+
+from typing import Set, Tuple
 
 import pytest
 
 
-def check_kernel_spec():
-    # https://jupyter-client.readthedocs.io/en/latest/api/kernelspec.html
-    import jupyter_client.kernelspec as jk
-
-    kernel_spec_manager = jk.KernelSpecManager()
-
-    print(kernel_spec_manager.get_all_specs())
-
-
-def _exec_notebook_nix(path):
+def _exec_notebook(path, tmp_path):
     """
     Run the ipynb file of path
     Raise exception if any error
@@ -27,81 +18,45 @@ def _exec_notebook_nix(path):
     # ijstokes et al, Command line execution of a jupyter notebook fails in default Anaconda 4.1, https://github.com/Anaconda-Platform/nb_conda_kernels/issues/34
     # obtain a temporary filename
     # https://docs.python.org/3/library/tempfile.html
-    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
-        # prepare a command running .ipynb file while converting
-        args = [
-            "jupyter", # name of program
-            "nbconvert", # option
-            "--to", "notebook", # conver to another ipynb file
-            "--execute", # run while convering
-            "--ExecutePreprocessor.timeout=1000",
-            "--ExecutePreprocessor.kernel_name=python",
-            "--output", fout.name, # output file name
-            path    # input file name
-        ]
-        # run the command above
-        # and raise an exception if error
-        subprocess.check_call(args)
+    # https://docs.pytest.org/en/7.1.x/how-to/tmp_path.html
 
-
-def _exec_notebook_win(path):
-    """
-    Run the ipynb file of path
-    Raise exception if any error
-    """
-    # http://nbconvert.readthedocs.io/en/latest/execute_api.html
-    # ijstokes et al, Command line execution of a jupyter notebook fails in default Anaconda 4.1, https://github.com/Anaconda-Platform/nb_conda_kernels/issues/34
-    # obtain a temporary filename
-    # https://docs.python.org/3/library/tempfile.html
-    ftemp = tempfile.NamedTemporaryFile(suffix=".ipynb")
-    filename = os.path.join(os.getcwd(), os.path.basename(ftemp.name))
-    ftemp.close()
-
-    # prepare a command running .ipynb file while converting
-    args = [
+    tmp_ipynb = (tmp_path / pathlib.Path(path).name).absolute()
+    args = (
         "jupyter", # name of program
         "nbconvert", # option
         "--to", "notebook", # conver to another ipynb file
         "--execute", # run while convering
-        "--ExecutePreprocessor.timeout=3600",
+        "--ExecutePreprocessor.timeout=1000",
         "--ExecutePreprocessor.kernel_name=python",
-        "--output", filename, # output file name
+        "--output", str(tmp_ipynb), # output file name
         path    # input file name
-    ]
-
-    try:
-        # run the command above
-        # and raise an exception if error
-        subprocess.check_call(args)
-    except BaseException as e:
-        print(e)
-        if os.path.exists(filename):
-            os.remove(filename)
-        raise e
-
-    print('success')
-    if os.path.exists(filename):
-        os.remove(filename)
+    )
+    # run the command above
+    # and raise an exception if error
+    subprocess.check_call(args, cwd=path.parent.absolute())
 
 
-# https://docs.pytest.org/en/latest/fixture.html#scope-sharing-a-fixture-instance-across-tests-in-a-class-module-or-session
-def get_exec_notebook():
-    # https://docs.python.org/3/library/platform.html#cross-platform
-    os_to_function_table = {
-        'posix': _exec_notebook_nix,
-        'nt': _exec_notebook_win,
-    }
-
-    return os_to_function_table.get(os.name, _exec_notebook_nix)
+@pytest.fixture
+def ignore_true() -> pathlib.Path:
+    return pathlib.Path('~/Documents/Python Scripts/proj/subfolder/.ipynb_checkpoints')
 
 
-def test_is_ignore():
+@pytest.fixture
+def ignore_true_in_env2() -> pathlib.Path:
+    return pathlib.Path('~/Documents/Python Scripts/proj/subfolder/')
 
-    true_00 = os.sep.join('~/Documents/Python Scripts/proj/subfolder/.ipynb_checkpoints'.split('/'))
-    assert is_ignore(true_00)
 
-    false_00 = os.sep.join('~/Documents/Python Scripts/proj/subfolder'.split('/'))
-    assert not is_ignore(false_00)
+@pytest.fixture
+def ignore_false_in_env2() -> pathlib.Path:
+    return pathlib.Path('~/Documents/Python Scripts/proj/')
+
+
+def test_is_ignore__true(ignore_true):
+    assert is_ignore(ignore_true)
+
+
+def test_is_ignore__false(ignore_true_in_env2):
+    assert not is_ignore(ignore_true_in_env2)
 
 
 @pytest.fixture(scope="function")
@@ -123,30 +78,23 @@ def env_ignore_folder_2():
         del os.environ['TEST_IPYNB_IGNORE_FOLDER']
 
 
-def test_is_ignore_env_ignore_folder(env_ignore_folder_2):
-    true_00 = os.sep.join('~/Documents/Python Scripts/proj/subfolder/.ipynb_checkpoints'.split('/'))
-    assert is_ignore(true_00)
+def test_is_ignore_env_ignore_folder(env_ignore_folder_2, ignore_true, ignore_true_in_env2, ignore_false_in_env2):
+    assert is_ignore(ignore_true)
 
-    true_01 = os.sep.join('~/Documents/Python Scripts/proj/subfolder'.split('/'))
-    assert is_ignore(true_01)
+    assert is_ignore(ignore_true_in_env2)
 
-    false_00 = os.sep.join('~/Documents/Python Scripts/proj/'.split('/'))
-    assert not is_ignore(false_00)
+    assert not is_ignore(ignore_false_in_env2)
 
 
-def is_ignore(path):
-    path_list = path.split(os.sep)
-
-    ignore_list = get_ignore_list()
-
-    return any(map(lambda path_part: path_part in ignore_list, path_list))
+def is_ignore(path:pathlib.Path) -> bool:
+    return (set(path.parts).intersection(get_ignore_set()))
 
 
 def test_get_ignore_list():
-    assert all(get_ignore_list())
+    assert all(get_ignore_set())
 
 
-def get_ignore_list():
+def get_ignore_set() -> Set[str]:
     ignore_list = ['.ipynb_checkpoints', '.git', '__pycache__', '.pytest_cache']
 
     if 'TEST_IPYNB_IGNORE_FOLDER' in os.environ:
@@ -154,48 +102,43 @@ def get_ignore_list():
 
     assert all(map(lambda path: isinstance(path, str), ignore_list))
 
-    return ignore_list
+    return set(ignore_list)
 
 
-def make_file_list(path='', ext='ipynb'):
+def get_filename_tuple(path='', ext='ipynb') -> Tuple[str]:
 
-    path = os.path.abspath(path)
+    path = pathlib.Path(path).absolute()
 
-    if not os.path.exists(path):
-        path=os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    if not path.exists():
+        print(f"{path} does not exist.", end=' ')
+        path = pathlib.Path(__file__).parent.parent.absolute()
+        print(f"Using {path}.")
 
-    file_list = []
-
-    # recursive loop
-    for root, _, filenames in os.walk(path):
-        if not (is_ignore(root)):
-            # files loop
-            for filename in filenames:
-                if os.path.splitext(filename)[-1].endswith(ext):
-                    file_path = os.path.join(root, filename)
-                    assert isinstance(file_path, str)
-                    assert os.path.exists(file_path), f"File Not Found {file_path}"
-                    file_list.append(file_path)
-
-    assert file_list, f"\npath = {path}\nget_ignore_list() = {get_ignore_list()}"
-
-    return file_list
+    return tuple(
+        map(
+            str,    # pytest param needs str for function name
+            filter(
+                lambda f_path: not is_ignore(f_path.parent.relative_to(path)),
+                path.glob(f'**/*.{ext}')
+            )        
+        )
+    )
 
 
-def test_make_file_list(env_ignore_folder_2):
+def test_get_filename_tuple(env_ignore_folder_2):
 
-    list_under_test = make_file_list()
+    tuple_under_test = get_filename_tuple()
 
-    assert list_under_test, "List empty"
+    assert tuple_under_test, "List empty"
 
-    for filename in list_under_test:
+    for filename in tuple_under_test:
         assert isinstance(filename, str)
         assert os.path.exists(filename), f"File Not Found {filename}"
-        assert not is_ignore(filename), f"Unexpected file {filename}"
+        assert not is_ignore(pathlib.Path(filename)), f"Unexpected file {filename}"
 
 
-def test_make_file_list_env_input(env_ignore_folder_2):
-    list_under_test = make_file_list(
+def test_get_filename_tuple__env_input(env_ignore_folder_2):
+    list_under_test = get_filename_tuple(
         os.path.abspath(
             os.environ.get('TEST_IPYNB_FOLDER', '')
         )
@@ -203,32 +146,28 @@ def test_make_file_list_env_input(env_ignore_folder_2):
 
     assert list_under_test, "List empty"
 
-    for filename in make_file_list(os.path.abspath(
+    for filename in get_filename_tuple(os.path.abspath(
                 os.environ.get('TEST_IPYNB_FOLDER', '')
             )):
         assert isinstance(filename, str)
         assert os.path.exists(filename), f"File Not Found {filename}"
-        assert not is_ignore(filename), f"Unexpected file {filename}"
+        assert not is_ignore(pathlib.Path(filename)), f"Unexpected file {filename}"
 
 
 # https://docs.pytest.org/en/latest/example/parametrize.html
 @pytest.mark.parametrize(
-    "filename, _exec_notebook",
-    itertools.zip_longest(
-        make_file_list(
-            os.path.abspath(
-                os.environ.get('TEST_IPYNB_FOLDER', '')
-            )
-        ),
-        [get_exec_notebook()], fillvalue=get_exec_notebook()
-    )
+    "filename", get_filename_tuple(
+        os.path.abspath(
+            os.environ.get('TEST_IPYNB_FOLDER', '')
+        )
+    ),
 )
 
 
-def test_ipynb_file(filename, _exec_notebook):
+def test_ipynb_file(filename, tmp_path):
     print('test() : %s' % filename)
     assert os.path.exists(filename), f"File Not Found {filename}"
-    _exec_notebook(filename)
+    _exec_notebook(pathlib.Path(filename), tmp_path)
 
 
 if "__main__" == __name__:
