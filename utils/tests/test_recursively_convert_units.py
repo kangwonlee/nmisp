ec89a6e0b4d9aa9d4796952cd5ec88d29697fbc2
KangWon LEE <kangwon@gmail.com>
2024-10-25 00:55:50 +0900

    add type hints

--- utils/tests/test_recursively_convert_units.py
+++ utils/tests/test_recursively_convert_units.py
@@ -60,18 +60,18 @@ def temp_proj_root(tmp_path:pathlib.Path):
     return root
 
 
-def test_os_walk_if_not_ignore__basic(temp_proj_root):
+def test_os_walk_if_not_ignore__basic(temp_proj_root:pathlib.Path):
     result_dirs = {pathlib.Path(root_name) for root_name, _, _ in rcu.os_walk_if_not_ignore(str(temp_proj_root))}
     expected_dirs = {temp_proj_root, temp_proj_root / "chapter1", temp_proj_root / "chapter2"}
     assert result_dirs == expected_dirs
 
 
-def test_os_walk_if_not_ignore__ignore(temp_proj_root):
+def test_os_walk_if_not_ignore__ignore(temp_proj_root:pathlib.Path):
     result_dirs = [pathlib.Path(root_name) for root_name, _, _ in rcu.os_walk_if_not_ignore(str(temp_proj_root))]
     assert temp_proj_root / "__pycache__" not in result_dirs
 
 
-def test_iter_ipynb_default_root(temp_proj_root, monkeypatch):
+def test_iter_ipynb_default_root(temp_proj_root:pathlib.Path, monkeypatch):
     """Test with the default project root."""
     monkeypatch.setattr(rcu, "get_proj_root", lambda: temp_proj_root)  # Patch get_proj_root
     expected_files = {
@@ -87,7 +87,7 @@ def test_iter_ipynb_default_root(temp_proj_root, monkeypatch):
     assert result_files == expected_files
 
 
-def test_iter_ipynb_specific_root(temp_proj_root):
+def test_iter_ipynb_specific_root(temp_proj_root:pathlib.Path):
     """Test with a specific root directory."""
     chapter2_path = temp_proj_root / "chapter2"
     expected_files = {chapter2_path / "notebook2.ipynb"}
@@ -100,7 +100,7 @@ def test_iter_ipynb_specific_root(temp_proj_root):
     assert result_files == expected_files
 
 
-def test_iter_ipynb_ignores_directories(temp_proj_root, monkeypatch):
+def test_iter_ipynb_ignores_directories(temp_proj_root:pathlib.Path, monkeypatch):
     """Test that ignored directories are skipped."""
     monkeypatch.setattr(rcu, "get_proj_root", lambda: temp_proj_root)
     result_files = map(
