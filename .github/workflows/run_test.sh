9768632a2087260d134afb63f969345746486b33
KangWon LEE <kangwon@gmail.com>
2025-02-07 19:14:26 +0900

    remove files not used or duplicate

--- .github/workflows/run_test.sh
+++
@@ -1,7 +0,0 @@
-. ./.github/workflows/set_os_env.sh
-
-echo "activate test-environment =================="
-source $MINICONDA_SUB_PATH/activate test-environment
-$MINICONDA_SUB_PATH/conda list
-
-$MINICONDA_PYTEST --numprocesses=auto -k 'not (links or update_nmisp_py)' tests/
